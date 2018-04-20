086f7d81205dd340248e88648c74e60a70767a75
risingone <anuragdhingra101@gmail.com>
2018-04-20 06:54:58 +0530

    Implementing the first route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,5 +5,7 @@ import Vapor
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
 public func routes(_ router: Router) throws {
-
+    router.get("name") { req in
+        return "Ethan Hunt"
+    }
 }
